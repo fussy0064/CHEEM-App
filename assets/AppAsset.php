69c4c0f83e59568d96f6ab19f5ad5a08294a3f46
CHEEM Dev <dev@cheem-app.local>
2026-09-17 08:02:48 +0000

    Fix news slider not sliding: load Bootstrap JS plugin bundle (needed for carousel)

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -15,5 +15,6 @@ class AppAsset extends AssetBundle
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap5\BootstrapAsset',
+        'yii\bootstrap5\BootstrapPluginAsset',
     ];
 }
